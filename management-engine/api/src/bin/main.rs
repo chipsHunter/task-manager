50f7e7c9fb8eb06b6f008cd2874dea5843b2c26f
hvorostok <hvorostok8@gmail.com>
2025-02-17 15:44:45 +0300

    feat: add pg connection

--- management-engine/api/src/bin/main.rs
+++ management-engine/api/src/bin/main.rs
@@ -1,16 +1,52 @@
+use std::{env, time::Duration};
+
 use api::auth::auth_routes;
 use axum::{
-    extract,
-    http::StatusCode,
-    response::{Html, IntoResponse, Response},
+    extract::{FromRef, FromRequestParts, State},
+    http::{request::Parts, StatusCode},
     routing::get,
     Router,
 };
+use dotenv::dotenv;
+use sqlx::postgres::{PgPool, PgPoolOptions};
+use tokio::net::TcpListener;
+
+// we can extract the connection pool with `State`
+async fn using_connection_pool_extractor(
+    State(pool): State<PgPool>,
+) -> Result<String, (StatusCode, String)> {
+    sqlx::query_scalar("select 'hello world from pg'")
+        .fetch_one(&pool)
+        .await
+        .map_err(internal_error)
+}
+
+fn internal_error<E>(err: E) -> (StatusCode, String)
+where
+    E: std::error::Error,
+{
+    (StatusCode::INTERNAL_SERVER_ERROR, err.to_string())
+}
 
 #[tokio::main]
 async fn main() {
+    dotenv().ok();
+
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .acquire_timeout(Duration::from_secs(3))
+        .connect(&env::var("DATABASE_URL").unwrap())
+        .await
+        .expect("can't connect to database");
+
     // build our application with some routes
-    let app = Router::new().nest("/auth", auth_routes());
+    let app = Router::new()
+        //.nest("/auth", auth_routes())
+        .route(
+            "/",
+            get(using_connection_pool_extractor).post(using_connection_pool_extractor),
+        )
+        .with_state(pool);
 
     // run it
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
